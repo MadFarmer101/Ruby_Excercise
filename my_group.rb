b6e048b6c75faf5e77580881e74715f46b6fb1f3
Janko <mailodjanka@gmail.com>
2020-01-14 22:32:34 +0100

    Array & Hashes Excercise

---
+++ my_group.rb
@@ -0,0 +1,5 @@
+my_group = []
+person_1 = {name: "Janko", gender:"male", age:"35"}
+person_2 = {name: "Mrgan", gender:"male", age:"36"}
+person_3 = {name: "Tomba", gender:"male", age:"34"}
+my_group = ["person_1", "person_2", "person3"]
