280e5cf6a5f831dc8edb83fd4b47b90a2b6f63c4
Janko <mailodjanka@gmail.com>
2020-01-17 17:22:43 +0100

    Movie year excercise, 1st Part

---
+++ movie_year_excercise.rb
@@ -0,0 +1,10 @@
+movies = {
+    "Pulp Fiction": 1994,
+    "Joker": 2019,
+    "Dogville": 2004,
+    "Full Metal Jacket": 1987,
+    "Donnie Darko": 2001
+}
+
+movies.each_value {|value| puts value}
+
