87a8d5ecdf8b48503ebc3af0dff9c9f1763353db
Janko <mailodjanka@gmail.com>
2020-01-18 23:22:09 +0100

    Finding specific value in hash

---
+++ hashes3.rb
@@ -0,0 +1,8 @@
+person = {name: 'Bob', occupation: 'web developer', hobby: 'painting'}
+
+#How to find a persons name? 
+person[:name]
+
+#Method to find a specific value
+
+person.has_value? ('painting')
