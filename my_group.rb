022237204294e2295bca51703b0266ebb10e7527
Janko <mailodjanka@gmail.com>
2020-01-18 15:43:24 +0100

    deleting files from this Repo

--- my_group.rb
+++
@@ -1,8 +0,0 @@
-my_group = [person_1 = {name: "Janko", gender: "man", age: 35},
-person_2 = {name: "Mrgan", gender: "man", age: 36},
-person_3 = {name: "Tomba", gender: "man", age: 34}
-]
-
-my_group.each do |person|
-    puts "#{person[:name]} is a #{person[:age]} years old #{person[:gender]}"
-end
