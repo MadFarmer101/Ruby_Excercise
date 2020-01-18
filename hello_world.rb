ec3f45a5e534d02f0ef6e406375acf4fecfba409
Janko <mailodjanka@gmail.com>
2020-01-18 20:10:50 +0100

    Adding files to this Repo

---
+++ hello_world.rb
@@ -0,0 +1,4 @@
+names = ['Janko', 'Helena', 'Nixon']
+names.each do |name|
+    puts "Hello #{name}"
+end
---
+++ name.rb
@@ -0,0 +1,4 @@
+puts "Hey, who are you?"
+name = gets.chop
+puts "Look I'm gonna write your name 10x times:"
+10.times {puts "#{name}"}
