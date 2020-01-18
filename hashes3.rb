c6dda2ddd7d6d7a4e416a69d1c075f509cc9dd0c
Janko <mailodjanka@gmail.com>
2020-01-18 23:42:48 +0100

    Difference beetwen ':' and '=>' in hashes

--- hashes3.rb
+++ hashes3.rb
@@ -6,3 +6,11 @@ person[:name]
 #Method to find a specific value
 
 person.has_value? ('painting')
+
+x = 'Hi there!'
+hash = {x: "some value"} 
+
+# Resault is {:x=>"some value"}
+
+hash2 = {x => "some value"} 
+#Resault is {"Hi there!"=>"some value"}. So, if we put a "=>" it will change the x with string we said that the x is equal to.
