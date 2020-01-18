4e826b99789fdfd2f349f4a48cf3ffa5780f2c03
Janko <mailodjanka@gmail.com>
2020-01-19 00:44:26 +0100

    #2Flow control exercise.Write a number from 1 to 100

--- flow_control.rb
+++ flow_control.rb
@@ -3,3 +3,13 @@ x = "Hello world"
 if x.bytesize > 10
     puts x.upcase
 end
+
+puts "Write me a number between 1 and 100"
+x = gets.chomp
+if x.to_i < 51
+    puts "Number is between 1 and 50"
+elsif x.to_i < 101
+    puts "Number is between 51 and 100"
+elsif x.to_i > 100
+    puts "I said number from 1 to 100!!"
+end
