9c3b6caad1bbc93cdd6a85f739458b39c3e12622
Janko <mailodjanka@gmail.com>
2020-01-19 01:34:55 +0100

    Def Greeting Exercise

--- flow_control.rb
+++ flow_control.rb
@@ -4,6 +4,7 @@ if x.bytesize > 10
     puts x.upcase
 end
 
+
 puts "Write me a number between 1 and 100"
 x = gets.chomp
 if x.to_i < 51
@@ -13,3 +14,12 @@ elsif x.to_i < 101
 elsif x.to_i > 100
     puts "I said number from 1 to 100!!"
 end
+
+
+
+def greeting(name)
+    puts "Hi, #{name}"
+end
+
+puts "write your name, please"
+greeting(gets)
